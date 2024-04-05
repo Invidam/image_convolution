81a955a3fac1604d91139421f2d5cfa60ef55feb
Chanyoung Park <idealproduction@naver.com>
2024-04-06 00:17:08 +0900

    fix: 이미지 출력에 실패하는 오류 해결

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <opencv2/opencv.hpp>
+#include <opencv2/core/utils/logger.hpp>
 #include <iostream>
 #include <filesystem>
 #include <vector>
@@ -6,9 +7,10 @@
 #include "Timer.h"
 
 namespace fs = std::filesystem;
+namespace cvl = cv::utils::logging;
 
 std::string getExtension(const std::string &filePath) {
-    return filePath.substr(filePath.find_last_of(".") + 1);
+    return filePath.substr(filePath.find_last_of('.') + 1);
 }
 
 std::string selectImageFromFolder() {
@@ -17,7 +19,7 @@ std::string selectImageFromFolder() {
     std::string extension;
 
     // List all files in the directory and filter for images
-    std::cout << "Listing all images in directory: " << imagesPath << std::endl;
+    std::cout << "Listing all images from directory: " << imagesPath << std::endl;
     for (const auto &entry: fs::directory_iterator(imagesPath)) {
         if (entry.is_regular_file()) {
             std::string filePath = entry.path().string();
@@ -32,12 +34,12 @@ std::string selectImageFromFolder() {
     }
 
     if (imageFiles.empty()) {
-        std::cerr << "No image files found in the directory." << std::endl;
+        std::cerr << "No image found." << std::endl;
         return "";
     }
 
     // Allow user to select an image
-    std::cout << "Enter the number of the image you wish to process: ";
+    std::cout << "Enter the number of image to process: ";
     size_t selection;
     std::cin >> selection;
     if (selection < 1 || selection > imageFiles.size()) {
@@ -49,7 +51,6 @@ std::string selectImageFromFolder() {
 }
 
 int measureBySteps(std::string selectedImage) {
-
     Timer fullTimer;
     Timer stepTimer;
     fullTimer.reset();
@@ -61,30 +62,39 @@ int measureBySteps(std::string selectedImage) {
         std::cerr << "Error loading image: " << selectedImage << std::endl;
         return -1;
     }
-    std::cout << "===========\n[1] Load Image.\nElapsed time: " << stepTimer.elapsed() << " ms" << std::endl;
+    std::cout << "===========\n[1] Importing image...\nElapsed time: " << stepTimer.elapsed() << " ms" << std::endl;
 
     stepTimer.reset();
     cv::Mat result = Calculator::calculate(image);
-    std::cout << "===========\n[2] Convolution Image.\nElapsed time: " << stepTimer.elapsed() << " ms" << std::endl;
+    std::cout << "===========\n[2] Applying convolution...\nElapsed time: " << stepTimer.elapsed() << " ms" << std::endl;
 
     stepTimer.reset();
+    fs::create_directory("output");
     std::string outputFilename = "output/" + fs::path(selectedImage).filename().string();
     std::cout << "Writing output to: " << outputFilename << std::endl;
     if (!cv::imwrite(outputFilename, result)) {
         std::cerr << "Failed to save the image." << std::endl;
         return -1;
     }
-    std::cout << "===========\n[3] Write Image.\nElapsed time: " << stepTimer.elapsed() << " ms" << std::endl;
+    std::cout << "===========\n[3] Writing image...\nElapsed time: " << stepTimer.elapsed() << " ms" << std::endl;
 
-    std::cout << "===========\n[F] Full process.\nElapsed time: " << fullTimer.elapsed() << " ms" << std::endl;
+    std::cout << "===========\nTotal process time: " << fullTimer.elapsed() << " ms" << std::endl;
     return 0;
 }
 
 int main() {
+    cvl::setLogLevel(cvl::LogLevel::LOG_LEVEL_WARNING);
     std::string selectedImage = selectImageFromFolder();
+    int result;
+
     if (selectedImage.empty()) {
-        return -1;
+        result = -1;
+    } else {
+        result = measureBySteps(selectedImage);
     }
 
-    return measureBySteps(selectedImage);
+    std::cout << "\nPress ENTER to exit...";
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin.get();
+    return result;
 }
