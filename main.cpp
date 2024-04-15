af35a7cd9056d45002a69eaa2e6fbcc01abea89d
Chanyoung Park <idealproduction@naver.com>
2024-04-15 18:49:01 +0900

    feat: OpenMP 버전 출력

--- main.cpp
+++ main.cpp
@@ -135,7 +135,8 @@ int measureBySteps(const std::string &selectedImage) {
 }
 
 int main() {
-    std::cout << "# of max threads: " << omp_get_max_threads() << std::endl;
+    std::cout << "# of max threads: " << omp_get_max_threads() << '\n';
+    std::cout << "OpenMP version: " << OPENMP_VERSION << std::endl;
     cvl::setLogLevel(cvl::LogLevel::LOG_LEVEL_WARNING);
     std::string selectedImage = selectImageFromFolder();
     int result;
